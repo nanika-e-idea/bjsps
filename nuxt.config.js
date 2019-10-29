6041cbbd2cdf51089392f5d14c05aab5ed5be67e
matsuge1Mb <smich.1982+stree@gmail.com>
2019-10-30 08:41:03 +0900

    コンポーネント作成中

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,7 +28,7 @@ export default {
   ** Plugins to load before mounting the App
   */
   plugins: [
-  //  '~/plugins/firebase.js'
+    '~/plugins/firebase.js'
   ],
   /*
   ** Nuxt.js dev-modules
--- pages/viewcmp.vue
+++ pages/viewcmp.vue
@@ -1,19 +1,27 @@
 <template>
   <div class="view">
-    <Button />
+    <Header />
+    <div class="container">
+      <Button />
+    </div>
+    <Footer />
   </div>
 </template>
 <script>
+import Header from '~/components/organisms/TheHeader.vue'
+import Footer from '~/components/organisms/TheFooter.vue'
 import Button from '~/components/atoms/AtomButton.vue'
 
 export default {
 components: {
-    Button
+    Header,
+    Footer,
+    Button,
   }
 }
 </script>
 <style scoped>
-.view{
+.container{
   box-sizing: border-box;
   width: 100%;
   padding: 20px;
